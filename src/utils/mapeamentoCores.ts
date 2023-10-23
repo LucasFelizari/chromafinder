9064c16ff05df6f4615b08962c089de69241456e
Theilor Stolfi <thstolfi@gmail.com>
2023-10-23 19:55:18 -0300

    fixed color names for easy understanding

--- src/utils/mapeamentoCores.ts
+++ src/utils/mapeamentoCores.ts
@@ -10,385 +10,183 @@ export type IMapeamentoCores = {
 export const mapeamentoCores: IMapeamentoCores[] = [
     {
         id: 1,
-        pt: "Vermelho",
-        en: "Red",
-        rgb: [255, 0, 0]
-    },
-    {
-        id: 2,
-        pt: "Verde",
-        en: "Green",
-        rgb: [0, 255, 0]
-    },
-    {
-        id: 3,
         pt: "Azul",
         en: "Blue",
         rgb: [0, 0, 255]
     },
     {
+        id: 2,
+        pt: "Azul Claro",
+        en: "Light Blue",
+        rgb: [173, 216, 230]
+    },
+    {
+        id: 3,
+        pt: "Azul Escuro",
+        en: "Dark Blue",
+        rgb: [0, 0, 139]
+    },
+    {
         id: 4,
+        pt: "Vermelho",
+        en: "Red",
+        rgb: [255, 0, 0]
+    },
+    {
+        id: 5,
+        pt: "Vermelho Claro",
+        en: "Light Red",
+        rgb: [255, 102, 102]
+    },
+    {
+        id: 6,
+        pt: "Vermelho Escuro",
+        en: "Dark Red",
+        rgb: [139, 0, 0]
+    },
+    {
+        id: 7,
         pt: "Amarelo",
         en: "Yellow",
         rgb: [255, 255, 0]
     },
     {
-        id: 5,
-        pt: "Rosa",
-        en: "Pink",
-        rgb: [255, 0, 255]
-    },
-    {
-        id: 6,
-        pt: "Ciano",
-        en: "Cyan",
-        rgb: [0, 255, 255]
-    },
-    {
-        id: 7,
-        pt: "Laranja",
-        en: "Orange",
-        rgb: [255, 165, 0]
-    },
-    {
         id: 8,
-        pt: "Roxo",
-        en: "Purple",
-        rgb: [128, 0, 128]
+        pt: "Amarelo Claro",
+        en: "Light Yellow",
+        rgb: [255, 255, 153]
     },
     {
         id: 9,
-        pt: "Marrom",
-        en: "Brown",
-        rgb: [139, 69, 19]
+        pt: "Amarelo Escuro",
+        en: "Dark Yellow",
+        rgb: [153, 153, 0]
     },
     {
         id: 10,
-        pt: "Preto",
-        en: "Black",
-        rgb: [0, 0, 0]
+        pt: "Verde",
+        en: "Green",
+        rgb: [0, 255, 0]
     },
     {
         id: 11,
-        pt: "Branco",
-        en: "White",
-        rgb: [255, 255, 255]
-    },
-    {
-        id: 12,
-        pt: "Cinza",
-        en: "Gray",
-        rgb: [128, 128, 128]
-    },
-    {
-        id: 13,
-        pt: "Prata",
-        en: "Silver",
-        rgb: [192, 192, 192]
-    },
-    {
-        id: 14,
-        pt: "Ouro",
-        en: "Gold",
-        rgb: [255, 215, 0]
-    },
-    {
-        id: 15,
-        pt: "Turquesa",
-        en: "Turquoise",
-        rgb: [64, 224, 208]
-    },
-    {
-        id: 16,
-        pt: "Violeta",
-        en: "Violet",
-        rgb: [238, 130, 238]
-    },
-    {
-        id: 17,
-        pt: "Indigo",
-        en: "Indigo",
-        rgb: [75, 0, 130]
-    },
-    {
-        id: 18,
-        pt: "Verde Limão",
-        en: "Lime Green",
-        rgb: [50, 205, 50]
-    },
-    {
-        id: 19,
-        pt: "Oliva",
-        en: "Olive",
-        rgb: [128, 128, 0]
-    },
-    {
-        id: 20,
-        pt: "Salmão",
-        en: "Salmon",
-        rgb: [250, 128, 114]
-    },
-    {
-        id: 21,
-        pt: "Verde Mar",
-        en: "Sea Green",
-        rgb: [46, 139, 87]
-    },
-    {
-        id: 22,
-        pt: "Chocolate",
-        en: "Chocolate",
-        rgb: [210, 105, 30]
-    },
-    {
-        id: 23,
-        pt: "Teal",
-        en: "Teal",
-        rgb: [0, 128, 128]
-    },
-    {
-        id: 24,
-        pt: "Azul Marinho",
-        en: "Navy Blue",
-        rgb: [0, 0, 128]
-    },
-    {
-        id: 25,
-        pt: "Bordô",
-        en: "Maroon",
-        rgb: [128, 0, 0]
-    },
-    {
-        id: 26,
-        pt: "Bege",
-        en: "Beige",
-        rgb: [245, 245, 220]
-    },
-    {
-        id: 27,
-        pt: "Coral",
-        en: "Coral",
-        rgb: [255, 127, 80]
-    },
-    {
-        id: 28,
-        pt: "Creme",
-        en: "Cream",
-        rgb: [255, 253, 208]
-    },
-    {
-        id: 29,
-        pt: "Cinza Claro",
-        en: "Light Gray",
-        rgb: [211, 211, 211]
-    },
-    {
-        id: 30,
-        pt: "Cinza Escuro",
-        en: "Dark Gray",
-        rgb: [169, 169, 169]
-    },
-    {
-        id: 31,
-        pt: "Verde Oliva",
-        en: "Olive Green",
-        rgb: [107, 142, 35]
-    },
-    {
-        id: 32,
-        pt: "Verde Musgo",
-        en: "Moss Green",
-        rgb: [173, 223, 173]
-    },
-    {
-        id: 33,
-        pt: "Verde Água",
-        en: "Aquamarine",
-        rgb: [127, 255, 212]
-    },
-    {
-        id: 34,
-        pt: "Verde Escuro",
-        en: "Dark Green",
-        rgb: [0, 100, 0]
-    },
-    {
-        id: 35,
         pt: "Verde Claro",
         en: "Light Green",
         rgb: [144, 238, 144]
     },
     {
-        id: 36,
-        pt: "Verde Amarelado",
-        en: "Chartreuse",
-        rgb: [127, 255, 0]
+        id: 12,
+        pt: "Verde Escuro",
+        en: "Dark Green",
+        rgb: [0, 100, 0]
     },
     {
-        id: 37,
-        pt: "Verde Azulado",
-        en: "Aquamarine",
-        rgb: [0, 255, 255]
+        id: 13,
+        pt: "Laranja",
+        en: "Orange",
+        rgb: [255, 165, 0]
     },
     {
-        id: 38,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Turquoise",
-        rgb: [0, 206, 209]
+        id: 14,
+        pt: "Laranja Claro",
+        en: "Light Orange",
+        rgb: [255, 179, 71]
     },
     {
-        id: 39,
-        pt: "Verde Azulado Claro",
-        en: "Light Turquoise",
-        rgb: [175, 238, 238]
+        id: 15,
+        pt: "Laranja Escuro",
+        en: "Dark Orange",
+        rgb: [255, 140, 0]
     },
     {
-        id: 40,
-        pt: "Verde Azulado Médio",
-        en: "Medium Turquoise",
-        rgb: [72, 209, 204]
+        id: 16,
+        pt: "Roxo",
+        en: "Purple",
+        rgb: [128, 0, 128]
     },
     {
-        id: 41,
-        pt: "Verde Azulado Pálido",
-        en: "Pale Turquoise",
-        rgb: [175, 238, 238]
+        id: 17,
+        pt: "Roxo Claro",
+        en: "Light Purple",
+        rgb: [204, 153, 255]
     },
     {
-        id: 42,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Slate Turquoise",
-        rgb: [0, 128, 128]
+        id: 18,
+        pt: "Roxo Escuro",
+        en: "Dark Purple",
+        rgb: [75, 0, 130]
     },
     {
-        id: 43,
-        pt: "Verde Azulado Médio",
-        en: "Medium Slate Turquoise",
-        rgb: [72, 209, 204]
+        id: 19,
+        pt: "Rosa",
+        en: "Pink",
+        rgb: [255, 192, 203]
     },
     {
-        id: 44,
-        pt: "Verde Azulado Pálido",
-        en: "Pale Turquoise",
-        rgb: [175, 238, 238]
+        id: 20,
+        pt: "Rosa Claro",
+        en: "Light Pink",
+        rgb: [255, 182, 193]
     },
     {
-        id: 45,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Slate Turquoise",
-        rgb: [0, 128, 128]
+        id: 21,
+        pt: "Rosa Escuro",
+        en: "Dark Pink",
+        rgb: [199, 21, 133]
     },
     {
-        id: 46,
-        pt: "Verde Azulado Médio",
-        en: "Medium Slate Turquoise",
-        rgb: [72, 209, 204]
+        id: 22,
+        pt: "Marrom",
+        en: "Brown",
+        rgb: [165, 42, 42]
     },
     {
-        id: 47,
-        pt: "Verde Azulado Pálido",
-        en: "Pale Turquoise",
-        rgb: [175, 238, 238]
+        id: 23,
+        pt: "Marrom Claro",
+        en: "Light Brown",
+        rgb: [210, 180, 140]
     },
     {
-        id: 48,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Slate Turquoise",
-        rgb: [0, 128, 128]
+        id: 24,
+        pt: "Marrom Escuro",
+        en: "Dark Brown",
+        rgb: [101, 67, 33]
     },
     {
-        id: 49,
-        pt: "Verde Azulado Médio",
-        en: "Medium Slate Turquoise",
-        rgb: [72, 209, 204]
+        id: 25,
+        pt: "Cinza",
+        en: "Gray",
+        rgb: [128, 128, 128]
     },
     {
-        id: 50,
-        pt: "Verde Azulado Pálido",
-        en: "Pale Turquoise",
-        rgb: [175, 238, 238]
+        id: 26,
+        pt: "Cinza Claro",
+        en: "Light Gray",
+        rgb: [211, 211, 211]
     },
     {
-        id: 51,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Slate Turquoise",
-        rgb: [0, 128, 128]
+        id: 27,
+        pt: "Cinza Escuro",
+        en: "Dark Gray",
+        rgb: [64, 64, 64]
     },
     {
-        id: 52,
-        pt: "Verde Azulado Médio",
-        en: "Medium Slate Turquoise",
-        rgb: [72, 209, 204]
+        id: 28,
+        pt: "Preto",
+        en: "Black",
+        rgb: [0, 0, 0]
     },
     {
-        id: 53,
-        pt: "Verde Azulado Pálido",
-        en: "Pale Turquoise",
-        rgb: [175, 238, 238]
+        id: 29,
+        pt: "Branco",
+        en: "White",
+        rgb: [255, 255, 255]
     },
     {
-        id: 54,
-        pt: "Verde Azulado Escuro",
-        en: "Dark Slate Turquoise",
-        rgb: [0, 128, 128]
-    },
-    {
-        id: 55,
-        pt: "Verde Azulado Médio",
-        en: "Medium Slate Turquoise",
-        rgb: [72, 209, 204]
-    },
+        id: 30,
+        pt: "Bege",
+        en: "Beige",
+        rgb: [245, 245, 220]
+    }
 ];
 
-// Cores
-
-// Vermelho: RGB(255, 0, 0)
-// Verde: RGB(0, 255, 0)
-// Azul: RGB(0, 0, 255)
-// Amarelo: RGB(255, 255, 0)
-// Rosa: RGB(255, 0, 255)
-// Ciano: RGB(0, 255, 255)
-// Laranja: RGB(255, 165, 0)
-// Roxo: RGB(128, 0, 128)
-// Marrom: RGB(139, 69, 19)
-// Preto: RGB(0, 0, 0)
-// Branco: RGB(255, 255, 255)
-// Cinza: RGB(128, 128, 128)
-// Prata: RGB(192, 192, 192)
-// Ouro: RGB(255, 215, 0)
-// Turquesa: RGB(64, 224, 208)
-// Violeta: RGB(238, 130, 238)
-// Indigo: RGB(75, 0, 130)
-// Verde Limão: RGB(50, 205, 50)
-// Oliva: RGB(128, 128, 0)
-// Salmão: RGB(250, 128, 114)
-// Verde Mar: RGB(46, 139, 87)
-// Chocolate: RGB(210, 105, 30)
-// Teal: RGB(0, 128, 128)
-// Coral: RGB(255, 127, 80)
-// Moçã: RGB(255, 228, 196)
-// Tomate: RGB(255, 99, 71)
-// Prata Antiga: RGB(192, 192, 192)
-// Turquesa Média: RGB(72, 209, 204)
-// Salmão Claro: RGB(255, 160, 122)
-// Púrpura Médio: RGB(147, 112, 219)
-// Marrom Médio: RGB(165, 42, 42)
-// Lima: RGB(0, 255, 0)
-// Rosa Quente: RGB(255, 105, 180)
-// Aqua: RGB(0, 255, 255)
-// Magenta: RGB(255, 0, 255)
-// Cinza Claro: RGB(211, 211, 211)
-// Salmão Claro: RGB(255, 160, 122)
-// Salmão Escuro: RGB(233, 150, 122)
-// Salmão Médio: RGB(222, 184, 135)
-// Salmão Escuro: RGB(233, 150, 122)
-// Verde Oliva Escuro: RGB(85, 107, 47)
-// Marrom Saddle: RGB(139, 69, 19)
-// Verde Oliva: RGB(128, 128, 0)
-// Amarelo Ouro Claro: RGB(250, 250, 210)
-// Amarelo Ouro Escuro: RGB(238, 232, 170)
-// Amarelo Ouro Médio: RGB(184, 134, 11)
-// Verde Mar Profundo: RGB(0, 128, 128)
-// Azul Dodger: RGB(30, 144, 255)
-// Rosa Choque: RGB(255, 0, 127)
-// Azul Pó: RGB(176, 224, 230)
